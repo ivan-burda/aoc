11a197808532f7c03f6d9c30ef83778e80b76446
ivan-burda <burda.ivan@protonmail.com>
2024-08-07 00:25:11 +0200

    2023-02-p2

--- 2023/02/2023-02.ts
+++ 2023/02/2023-02.ts
@@ -5,7 +5,7 @@ const availableCubes:Record<string, number> = {
     blue: 14
 }
 
-export const getSum = (upTo:number):number => {
+export const getSumOfRange = (upTo:number):number => {
     let sum = 0;
     for (let i = 0; i <=upTo; i++) {
         sum+=i;
@@ -13,19 +13,24 @@ export const getSum = (upTo:number):number => {
     return sum
 }
 
-export const possibleGameIdsSum = (input:string) => {
-    const impossibleGameIndices = new Set<number>();
-
-   const games = input.split('\n').map((line) => {
+const getGamesRepresentation = (input:string) => {
+    return input.split('\n').map((line) => {
         const [,drawCubes] = line.split(': ')
         return drawCubes.split('; ').map(draw=> {
             return draw.split(', ').map(colorAndCount=>{
-               const [count, color] = colorAndCount.split(' ');
+                const [count, color] = colorAndCount.split(' ');
                 return({color, count})
             });
         })
     })
 
+}
+
+
+export const possibleGameIdsSum = (input:string):number => {
+    const impossibleGameIndices = new Set<number>();
+
+    const games = getGamesRepresentation(input);
     games.forEach(((game,index)=>game.forEach(draw=>draw.forEach(colorAndCount=>{
         const {color, count} = colorAndCount;
         if(Number(count) > availableCubes[color]){
@@ -33,5 +38,27 @@ export const possibleGameIdsSum = (input:string) => {
         }
     }))))
 
-    return getSum(games.length) - Array.from(impossibleGameIndices).reduce((sum, currentNumber)=>sum + currentNumber, 0);
+    return getSumOfRange(games.length) - Array.from(impossibleGameIndices).reduce((sum, currentNumber)=>sum + currentNumber, 0);
+}
+
+export const sumPowerOfCubeSetsForGames = (input:string):number => {
+    let sum = 0;
+    const games = getGamesRepresentation(input);
+
+    games.forEach(game=>{
+        const minReq:Record<string, number> = {};
+        game.forEach(draw=>{
+            draw.forEach(colorCount=>{
+                if(!(colorCount.color in minReq)){
+                    minReq[colorCount.color]=Number(colorCount.count)
+                }
+                if(colorCount.color in minReq && Number(colorCount.count) > minReq[colorCount.color]){
+                    minReq[colorCount.color] = Number(colorCount.count);
+                }
+            })
+        })
+        sum += Object.values(minReq).reduce((acc, cur)=>acc * cur, 1);;
+    })
+
+    return sum;
 }
--- 2023/02/__tests__/2023-02.test.ts
+++ 2023/02/__tests__/2023-02.test.ts
@@ -1,10 +1,7 @@
-import {getSum, possibleGameIdsSum} from "../2023-02";
+import {possibleGameIdsSum, sumPowerOfCubeSetsForGames} from "../2023-02";
 import {readFileSync} from "graceful-fs";
 
 describe('AOC-2023-02', () => {
-    it('should return sum', () => {
-        expect(getSum(5)).toEqual(15)
-    });
     it('part1-test data', () => {
         const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -17,5 +14,20 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
     it('part1-real data',()=>{
         const input = readFileSync('2023/02/2023-02.txt', 'utf-8');
         expect(possibleGameIdsSum(input)).toBe(2369);
+    })
+
+    it('part2 test data', () => {
+        const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+
+        expect(sumPowerOfCubeSetsForGames(input)).toBe(2286);
+    });
+
+    it('part2 real data', () => {
+        const input = readFileSync('2023/02/2023-02.txt', 'utf-8');
+        expect(sumPowerOfCubeSetsForGames(input)).toBe(66363);
     })
 });
