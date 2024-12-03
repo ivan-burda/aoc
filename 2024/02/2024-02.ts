a4b48c272f5c925ff885a004b26a37b526a09d9a
ivan-burda <burda.ivan@protonmail.com>
2024-12-03 01:11:13 +0100

    2024-02-p1

--- 2024/02/2024-02.ts
+++ 2024/02/2024-02.ts
@@ -1,38 +1,66 @@
-//first 2 digit relations set a inc/decr trend which must not be violated when going on in the report
-//two adjacent digits in a report can differ by 1,2 or 3 max
-
-export const getSafeReportsCount = (input: string): number => {
-  const results: boolean[] = [];
-  const reports = input
+const getReports = (input: string): number[][] => {
+  return input
     .split("\n")
     .map((report) => report.split(" "))
     .map((report) => report.map(Number));
+};
 
-  reports.forEach((report) => {
-    let isReportSafe = true;
-    const isReportIncreasing = report[1] - report[0] > 0;
-    for (let i = 0; i < report.length - 1; i++) {
-      if (report[i + 1] === report[i]) {
-        isReportSafe = false;
-        return;
-      }
-
-      const isAdjacentPairIncreasing = report[i + 1] - report[i] > 0;
-      if (isAdjacentPairIncreasing !== isReportIncreasing) {
-        isReportSafe = false;
-        return;
-      }
-
-      const adjacentPairAbsoluteDifference = Math.abs(
-        report[i + 1] - report[i],
-      );
-      if (adjacentPairAbsoluteDifference > 3) {
-        isReportSafe = false;
-        return;
-      }
+export const isIncreasing = (report: number[]): boolean => {
+  for (let i = 0; i < report.length; i++) {
+    if (report[i + 1] < report[i]) {
+      return false;
     }
-    results.push(isReportSafe);
+  }
+  return true;
+};
+
+export const isDecreasing = (report: number[]): boolean => {
+  for (let i = 0; i < report.length; i++) {
+    if (report[i + 1] > report[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const isRequiredAllowedDifference = (
+  num1: number,
+  num2: number,
+): boolean => {
+  const absDiff = Math.abs(num1 - num2);
+  return absDiff >= 1 && absDiff <= 3;
+};
+
+const isReportSafe = (report: number[]): boolean => {
+  const isReportIncreasing = isIncreasing(report);
+  const isReportDecreasing = isDecreasing(report);
+  const hasReportConstantTrend = isReportDecreasing || isReportIncreasing;
+  let areAdjacentDifferencesOK = true;
+
+  for (let i = 0; i < report.length - 1; i++) {
+    if (!isRequiredAllowedDifference(report[i], report[i + 1])) {
+      areAdjacentDifferencesOK = false;
+    }
+  }
+
+  return hasReportConstantTrend && areAdjacentDifferencesOK;
+};
+
+export const getSafeReportsCountPart1 = (input: string): number => {
+  let safeReports = 0;
+  const reports = getReports(input);
+  reports.forEach((report) => {
+    if (isReportSafe(report)) {
+      safeReports++;
+    }
   });
 
-  return results.length;
+  return safeReports;
+};
+
+// Part2
+export const getSafeReportsCountPart2 = (input: string): number => {
+  const completelySafeReports = getSafeReportsCountPart1(input);
+
+  return completelySafeReports;
 };
--- 2024/02/__tests__/2024-02.test.ts
+++ 2024/02/__tests__/2024-02.test.ts
@@ -1,4 +1,10 @@
-import { getSafeReportsCount } from "../2024-02";
+import {
+  getSafeReportsCountPart1,
+  getSafeReportsCountPart2,
+  isDecreasing,
+  isIncreasing,
+  isRequiredAllowedDifference,
+} from "../2024-02";
 import { readFileSync } from "graceful-fs";
 
 const testInput = `7 6 4 2 1
@@ -10,20 +16,58 @@ const testInput = `7 6 4 2 1
 
 const fullInput = readFileSync("2024/02/2024-02.txt", "utf8");
 
+describe("isIncreasing", () => {
+  it("true", () => {
+    expect(isIncreasing([0, 1, 2, 3, 4, 5])).toBe(true);
+  });
+  it("false", () => {
+    expect(isIncreasing([0, 1, 2, 3, 2, 5])).toBe(false);
+  });
+});
+
+describe("isDecreasing", () => {
+  it("true", () => {
+    expect(isDecreasing([5, 4, 3, 2, 1, 0])).toBe(true);
+  });
+  it("false", () => {
+    expect(isDecreasing([5, 4, 3, 4, 1, 0])).toBe(false);
+  });
+});
+
+describe("isRequiredAllowedDifference", () => {
+  it("true", () => {
+    expect(isRequiredAllowedDifference(0, 3)).toBe(true);
+  });
+  it("true", () => {
+    expect(isRequiredAllowedDifference(0, 1)).toBe(true);
+  });
+  it("false", () => {
+    expect(isRequiredAllowedDifference(1, 1)).toBe(false);
+  });
+  it("false", () => {
+    expect(isRequiredAllowedDifference(0, 4)).toBe(false);
+  });
+});
+
 describe("AOC-2024-02", () => {
   it("part1-test: returns safe-report count", () => {
-    expect(getSafeReportsCount(testInput)).toEqual(2);
+    expect(getSafeReportsCountPart1(testInput)).toEqual(2);
   });
 
   it("part1-full: returns safe-report count", () => {
-    expect(getSafeReportsCount(fullInput)).toEqual(252);
+    expect(getSafeReportsCountPart1(fullInput)).toEqual(252);
   });
 
   it("part2-test: returns safe-report count", () => {
-    expect(getSafeReportsCount(testInput)).toEqual(4);
+    expect(getSafeReportsCountPart2(testInput)).toEqual(4);
   });
 
   it("part2-full: returns safe-report count", () => {
-    expect(getSafeReportsCount(fullInput)).toEqual(999);
+    // expect(getSafeReportsCount2(fullInput)).not.toEqual(68);
+    // expect(getSafeReportsCount2(fullInput)).not.toEqual(65);
+    // expect(getSafeReportsCount2(fullInput)).not.toEqual(294); - too low
+    // expect(getSafeReportsCount2(fullInput)).not.toEqual(302); - too low
+    // expect(getSafeReportsCount2(fullInput)).not.toEqual(317); - too low
+    expect(getSafeReportsCountPart2(fullInput)).toEqual(294);
   });
 });
