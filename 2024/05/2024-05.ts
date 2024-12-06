50ef6207e1031e739893d32044deb80131b1bfdd
ivan-burda <burda.ivan@protonmail.com>
2024-12-06 01:27:59 +0100

    2024-05 refactor

--- 2024/05/2024-05.ts
+++ 2024/05/2024-05.ts
@@ -2,6 +2,7 @@ import { Dictionary } from "../../utils/dataStructures/dictionary/dictionary";
 
 type Rule = string[];
 type Update = string[];
+type PagePositionsInUpdate = Dictionary<string, number>;
 
 const getRules = (rulesInput: string): Rule[] => {
   return rulesInput.split("\n").map((line) => line.split("|"));
@@ -11,36 +12,120 @@ const getUpdates = (updatesInput: string): Update[] => {
   return updatesInput.split("\n").map((line) => line.split(","));
 };
 
+const getDictionaryOfUpdatePagePositions = (update: Update) => {
+  const pagePositions = new Dictionary<string, number>();
+  update.forEach((page, index) => pagePositions.add(page, index));
+  return pagePositions;
+};
+
+const getRulesRelevantForUpdate = (
+  rules: Rule[],
+  pagePositionsInUpdate: PagePositionsInUpdate,
+): Rule[] => {
+  return rules.filter(([page1, page2]) => {
+    const isPage1IncludedInRule = pagePositionsInUpdate.containsKey(page1);
+    const isPage2IncludedInRule = pagePositionsInUpdate.containsKey(page2);
+    return isPage1IncludedInRule && isPage2IncludedInRule;
+  });
+};
+
+const getViolatedRelevantRules = (
+  relevantRules: Rule[],
+  pagePositionsInUpdate: PagePositionsInUpdate,
+): Rule[] => {
+  return relevantRules.filter(([page1, page2]) => {
+    const p1Index = pagePositionsInUpdate.get(page1) ?? -1;
+    const p2Index = pagePositionsInUpdate.get(page2) ?? -1;
+    return p1Index > p2Index;
+  });
+};
+
+const getCorrectUpdates = (updates: Update[], rules: Rule[]) => {
+  return updates.filter((update) => {
+    const pagePositionsInUpdate = getDictionaryOfUpdatePagePositions(update);
+    const relevantRules = getRulesRelevantForUpdate(
+      rules,
+      pagePositionsInUpdate,
+    );
+    const violatedRelevantRules = getViolatedRelevantRules(
+      relevantRules,
+      pagePositionsInUpdate,
+    );
+
+    return violatedRelevantRules.length === 0;
+  });
+};
+
+const getIncorrectUpdates = (updates: Update[], rules: Rule[]) => {
+  return updates.filter((update) => {
+    const pagePositionsInUpdate = getDictionaryOfUpdatePagePositions(update);
+    const relevantRules = getRulesRelevantForUpdate(
+      rules,
+      pagePositionsInUpdate,
+    );
+    const violatedRelevantRules = getViolatedRelevantRules(
+      relevantRules,
+      pagePositionsInUpdate,
+    );
+    return violatedRelevantRules.length > 0;
+  });
+};
+
+const getCorrectedUpdates = (
+  incorrectUpdates: Update[],
+  rules: Rule[],
+): Update[] => {
+  return incorrectUpdates.map((update) => {
+    const pagePositionsInUpdate = getDictionaryOfUpdatePagePositions(update);
+    const relevantRules = getRulesRelevantForUpdate(
+      rules,
+      pagePositionsInUpdate,
+    );
+
+    const ruleFirstNumbers = relevantRules.map(([num1]) => num1);
+    const ruleSecondNumbers = relevantRules.map(([, num2]) => num2);
+    const correctedUpdateLastNumber = ruleSecondNumbers.filter(
+      (num) => !ruleFirstNumbers.includes(num),
+    )[0];
+
+    const rulesFirstNumbersAndTheirCounts = Object.entries(
+      ruleFirstNumbers.reduce<Record<string, number>>(
+        (countMap, currentNumber) => {
+          if (!countMap[currentNumber]) {
+            countMap[currentNumber] = 1;
+          } else {
+            countMap[currentNumber]++;
+          }
+          return countMap;
+        },
+        {},
+      ),
+    );
+
+    const ruleFirstNumbersOrderedByTheirCounts = rulesFirstNumbersAndTheirCounts
+      .sort((a, b) => b[1] - a[1])
+      .map(([num1]) => num1);
+
+    return [...ruleFirstNumbersOrderedByTheirCounts, correctedUpdateLastNumber];
+  });
+};
+
+const sumUpMiddleNumbers = (updates: Update[]): number => {
+  return updates.reduce((total, currentUpdate) => {
+    total = total + Number(currentUpdate[(currentUpdate.length - 1) / 2]);
+    return total;
+  }, 0);
+};
+
 //part1
 export const getMiddlePageSumForCorrectUpdates = (
   rulesInput: string,
   updatesInput: string,
 ): number => {
-  const rules = getRules(rulesInput);
   const updates = getUpdates(updatesInput);
-  const validUpdates = updates.filter((update) => {
-    const pagePositionDict = new Dictionary<string, number>();
-    update.forEach((page, index) => pagePositionDict.add(page, index));
-
-    const relevantRules = rules.filter(([page1, page2]) => {
-      const isPage1IncludedInRule = pagePositionDict.containsKey(page1);
-      const isPage2IncludedInRule = pagePositionDict.containsKey(page2);
-      return isPage1IncludedInRule && isPage2IncludedInRule;
-    });
-
-    const violatedRelevantRules = relevantRules.filter(([page1, page2]) => {
-      const p1Index = pagePositionDict.get(page1) ?? -1;
-      const p2Index = pagePositionDict.get(page2) ?? -1;
-      return p1Index > p2Index;
-    });
-
-    return violatedRelevantRules.length === 0;
-  });
-
-  return validUpdates.reduce((total, currentUpdate) => {
-    total = total + Number(currentUpdate[(currentUpdate.length - 1) / 2]);
-    return total;
-  }, 0);
+  const rules = getRules(rulesInput);
+  const correctUpdates = getCorrectUpdates(updates, rules);
+  return sumUpMiddleNumbers(correctUpdates);
 };
 
 //part2
@@ -48,67 +133,9 @@ export const getMiddlePageSumForCorrectedUpdates = (
   rulesInput: string,
   updatesInput: string,
 ): number => {
-  const rules = getRules(rulesInput);
   const updates = getUpdates(updatesInput);
-
-  const incorrectUpdates = updates.filter((update) => {
-    const pagePositionDict = new Dictionary<string, number>();
-    update.forEach((page, index) => pagePositionDict.add(page, index));
-
-    const relevantRules = rules.filter(([page1, page2]) => {
-      const isPage1IncludedInRule = pagePositionDict.containsKey(page1);
-      const isPage2IncludedInRule = pagePositionDict.containsKey(page2);
-      return isPage1IncludedInRule && isPage2IncludedInRule;
-    });
-
-    const violatedRelevantRules = relevantRules.filter(([page1, page2]) => {
-      const p1Index = pagePositionDict.get(page1) ?? -1;
-      const p2Index = pagePositionDict.get(page2) ?? -1;
-      return p1Index > p2Index;
-    });
-
-    return violatedRelevantRules.length > 0;
-  });
-
-  const correctedUpdates = incorrectUpdates.map((update) => {
-    const pagePositionDict = new Dictionary<string, number>();
-    update.forEach((page, index) => pagePositionDict.add(page, index));
-
-    const relevantRules = rules.filter(([page1, page2]) => {
-      const isPage1IncludedInRule = pagePositionDict.containsKey(page1);
-      const isPage2IncludedInRule = pagePositionDict.containsKey(page2);
-      return isPage1IncludedInRule && isPage2IncludedInRule;
-    });
-
-    const ruleFirstNumbers = relevantRules.map(([num1]) => num1);
-    const ruleSecondNumbers = relevantRules.map(([, num2]) => num2);
-
-    const correctedUpdateLastNumber = Number(
-      ruleSecondNumbers.filter((num) => !ruleFirstNumbers.includes(num))[0],
-    );
-
-    return [
-      ...Object.entries(
-        ruleFirstNumbers.reduce<Record<string, number>>(
-          (countMap, currentNumber) => {
-            if (!countMap[currentNumber]) {
-              countMap[currentNumber] = 1;
-            } else {
-              countMap[currentNumber]++;
-            }
-            return countMap;
-          },
-          {},
-        ),
-      )
-        .sort((a, b) => b[1] - a[1])
-        .map(([num1]) => Number(num1)),
-      correctedUpdateLastNumber,
-    ];
-  });
-
-  return correctedUpdates.reduce((total, currentUpdate) => {
-    total = total + Number(currentUpdate[(currentUpdate.length - 1) / 2]);
-    return total;
-  }, 0);
+  const rules = getRules(rulesInput);
+  const incorrectUpdates = getIncorrectUpdates(updates, rules);
+  const correctedUpdates = getCorrectedUpdates(incorrectUpdates, rules);
+  return sumUpMiddleNumbers(correctedUpdates);
 };
