960aeba0ad8a987857824275b857ec0f1860b045
ivan-burda <burda.ivan@protonmail.com>
2024-12-06 21:52:45 +0100

    2024-06 init

---
+++ 2024/06/2024-06.ts
@@ -0,0 +1,3 @@
+export const getCountOfVisitedPositions = (input: string): number => {
+  return 0;
+};
--- 2024/06/__tests__/2024-06.test.ts
+++ 2024/06/__tests__/2024-06.test.ts
@@ -1,1 +1,24 @@
-describe('AOC-2024-06', () => {});
+import { readFileSync } from "graceful-fs";
+import { getCountOfVisitedPositions } from "../2024-06";
+
+const testInput = `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`;
+
+const fullInput = readFileSync("2024/06/2024-06-rules.txt", "utf8");
+
+describe("AOC-2024-06", () => {
+  it("part1-test: returns counts of positions the guard visits before leaving the map", () => {
+    expect(getCountOfVisitedPositions(testInput)).toBe(41);
+  });
+  it("part2-full: returns counts of positions the guard visits before leaving the map", () => {
+    expect(getCountOfVisitedPositions(fullInput)).toBe(1);
+  });
+});
