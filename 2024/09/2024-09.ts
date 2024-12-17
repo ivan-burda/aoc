7a6fdbcb7f89e8feb5a8738ac1c26c2c21b3ac80
ivan-burda <burda.ivan@protonmail.com>
2024-12-18 00:50:13 +0100

    2024-09-part2 wip

--- 2024/09/2024-09.ts
+++ 2024/09/2024-09.ts
@@ -1,4 +1,5 @@
 const countChecksum = (reorderedBlocks: string[]): number => {
+  console.log(reorderedBlocks);
   return reorderedBlocks.reduce((acc, curr, index) => {
     acc += Number(curr) * index;
     return acc;
@@ -62,14 +63,54 @@ export const getChecksumPart1 = (input: string): number => {
   return countChecksum(reorderedBlocks);
 };
 
-const getSameNumberBlocks = (
-  input: string,
-): { block: string; startIndex: number }[] => {
-  const matches = [...input.matchAll(/(\d)\1*/g)];
-  return matches.map((match) => ({
-    block: match[0],
-    startIndex: match.index,
-  }));
+//////
+interface Block {
+  block: string;
+  length: number;
+  index: number;
+  originalIndex: number;
+}
+
+export const getBlocks2 = (input: string): Block[] => {
+  return input.split("").reduce<Block[]>((acc, currentBlock, currentIndex) => {
+    if (currentIndex % 2 === 1) {
+      if (currentBlock !== "0") {
+        let newBlock = "";
+        for (let i = 0; i < Number(currentBlock); i++) {
+          newBlock += String(".");
+        }
+        acc.push({
+          block: newBlock,
+          length: newBlock.length,
+          index: currentIndex,
+          originalIndex: currentIndex / 2,
+        });
+      } else {
+        acc.push({
+          block: "noGap",
+          length: 0,
+          index: currentIndex,
+          originalIndex: currentIndex / 2,
+        });
+      }
+    } else {
+      let newBlock = "";
+      for (let i = 0; i < Number(currentBlock); i++) {
+        newBlock += String(String(currentIndex / 2));
+      }
+      acc.push({
+        block: newBlock,
+        length: newBlock.length,
+        index: currentIndex,
+        originalIndex: currentIndex / 2,
+      });
+    }
+    return acc;
+  }, []);
+};
+
+export const getSameNumberBlocks2 = (input: string): string[] => {
+  return [];
 };
 
 interface EmptySlot {
@@ -100,65 +141,140 @@ const getEmptySlots = (blocks: string[]): EmptySlot[] => {
   return emptySlots;
 };
 
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+interface EmptyBlockInfo {
+  block: string;
+  length: number;
+  index: number;
+  originalIndex: number;
+}
+
+interface SameNumberBlockInfo {
+  block: string;
+  length: number;
+  index: number;
+  originalIndex: number;
+}
+
+const getFirstSuitableEmptyBlock = (
+  blocks: Block[],
+  minCapacity: number,
+): EmptyBlockInfo | undefined => {
+  return blocks.find(
+    (block) => block.length >= minCapacity && block.block.includes("."),
+  );
+};
+
+const getSameNumberBlocks = (blocks: Block[]): SameNumberBlockInfo[] => {
+  const results = [];
+  for (let i = blocks.length - 1; i >= 0; i--) {
+    if (!blocks[i].block.includes(".") && !blocks[i].block.includes("noGap")) {
+      results.push({
+        block: blocks[i].block,
+        length: blocks[i].block.length,
+        index: i,
+        originalIndex: blocks[i].originalIndex,
+      });
+    }
+  }
+  return results;
+};
+
+const getEmptySpace = (length: number): string => {
+  let remainingSpace = "";
+  for (let i = 0; i < length; i++) {
+    remainingSpace += ".";
+  }
+  return remainingSpace;
+};
+
+const getRemainingEmptyBlockSpaces = (
+  sameNumberBlock: SameNumberBlockInfo,
+  firstSuitableEmptyBloc: EmptyBlockInfo,
+): string => {
+  const difference = firstSuitableEmptyBloc.length - sameNumberBlock.length;
+  if (difference === 0) {
+    return "";
+  }
+  return getEmptySpace(difference);
+};
+
+const recreateAsString = (blocks: Block[]): string => {
+  return blocks.reduce((final, curr) => {
+    if (curr.block === "noGap") {
+      return final;
+    }
+    final += curr.block;
+    return final;
+  }, "");
+};
+
 const countChecksum2 = (reorderedBlocks: string[]): number => {
+  console.log(reorderedBlocks);
   return reorderedBlocks.reduce((acc, curr, index) => {
-    if (numbers.some((num) => num === curr)) {
+    if (!curr.includes(".")) {
       acc += Number(curr) * index;
     }
     return acc;
   }, 0);
 };
 
+function splitStringByLength(str: string, chunkSize: number): string[] {
+  const result = [];
+  for (let i = 0; i < str.length; i += chunkSize) {
+    result.push(str.slice(i, i + chunkSize));
+  }
+  return result;
+}
+
 //part2
 export const getChecksumPart2 = (input: string): number => {
-  const blocks = getBlocks(input);
-  const sameNumberBlocks = getSameNumberBlocks(blocks.join(""));
-
-  for (
-    let numberBlocksIterator = sameNumberBlocks.length - 1;
-    numberBlocksIterator >= 0;
-    numberBlocksIterator--
-  ) {
-    const emptySlots = getEmptySlots(blocks);
-    const currentNumberBlockLength =
-      sameNumberBlocks[numberBlocksIterator].block.length;
-    const firstAvailableEmptySlot = emptySlots.findIndex(
-      (slot) => slot.length >= currentNumberBlockLength,
+  let blocks = getBlocks2(input);
+  const sameNumberBlocks = getSameNumberBlocks(blocks);
+  let joinedBlocks: string[] = [];
+  for (let i = 0; i < sameNumberBlocks.length; i++) {
+    const sameNumberBlock = getSameNumberBlocks(blocks)[i];
+    const firstSuitableEmptyBlock = getFirstSuitableEmptyBlock(
+      blocks,
+      sameNumberBlock.length,
     );
-    if (
-      firstAvailableEmptySlot >= 0 &&
-      emptySlots[firstAvailableEmptySlot].startIndex <
-        sameNumberBlocks[numberBlocksIterator].startIndex
-    ) {
-      for (
-        let numberBlockLengthIterator = 0;
-        numberBlockLengthIterator <
-        sameNumberBlocks[numberBlocksIterator].block.length;
-        numberBlockLengthIterator++
-      ) {
-        [
-          blocks[
-            emptySlots[firstAvailableEmptySlot].startIndex +
-              numberBlockLengthIterator
-          ],
-          blocks[
-            sameNumberBlocks[numberBlocksIterator].startIndex +
-              numberBlockLengthIterator
-          ],
-        ] = [
-          blocks[
-            sameNumberBlocks[numberBlocksIterator].startIndex +
-              numberBlockLengthIterator
-          ],
-          blocks[
-            emptySlots[firstAvailableEmptySlot].startIndex +
-              numberBlockLengthIterator
-          ],
-        ];
+    if (firstSuitableEmptyBlock) {
+      const remainingEmptyBlockSpaces = getRemainingEmptyBlockSpaces(
+        sameNumberBlock,
+        firstSuitableEmptyBlock,
+      );
+      firstSuitableEmptyBlock.length = sameNumberBlock.length;
+      firstSuitableEmptyBlock.block = getEmptySpace(sameNumberBlock.length);
+      const indexToPlaceTheNumbersTo = blocks.findIndex(
+        (item) => item.index === firstSuitableEmptyBlock.index,
+      );
+
+      const indexToPlaceTheEmptySpacesTo = sameNumberBlock.index;
+      if (indexToPlaceTheNumbersTo < indexToPlaceTheEmptySpacesTo) {
+        blocks[indexToPlaceTheNumbersTo] = sameNumberBlock;
+        blocks[indexToPlaceTheEmptySpacesTo] = firstSuitableEmptyBlock;
+
+        if (remainingEmptyBlockSpaces.length > 0) {
+          blocks.splice(indexToPlaceTheNumbersTo + 1, 0, {
+            block: remainingEmptyBlockSpaces,
+            length: remainingEmptyBlockSpaces.length,
+            index: indexToPlaceTheNumbersTo + 1,
+            originalIndex: -1,
+          });
+        }
+
+        joinedBlocks = blocks
+          .filter((item) => item.block !== "noGap")
+          .flatMap((item) => {
+            if (item.block.includes(".")) {
+              return String(item.block);
+            }
+
+            return item.block;
+          });
       }
     }
   }
-  // console.log(blocks);
-  return countChecksum2(blocks);
+  const result = countChecksum2(joinedBlocks);
+  console.log(result);
+  return result;
 };
