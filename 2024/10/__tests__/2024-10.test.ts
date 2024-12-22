63046677f5c2ab202df57c96eaf093cf626bdf00
ivan-burda <burda.ivan@protonmail.com>
2024-12-22 22:14:00 +0100

    2024-10-p2-test-setup

--- 2024/10/__tests__/2024-10.test.ts
+++ 2024/10/__tests__/2024-10.test.ts
@@ -34,6 +34,38 @@ const testInput4 = `89010123
 01329801
 10456732`;
 
+const part2TestInput1 = `.....0.
+..4321.
+..5..2.
+..6543.
+..7..4.
+..8765.
+..9....`;
+
+const part2TestInput2 = `..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....`;
+
+const part2TestInput3 = `012345
+123456
+234567
+345678
+4.6789
+56789.`;
+
+const part2TestInput4 = `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`;
+
 const fullInput = readFileSync("2024/10/2024-10.txt", "utf8");
 
 describe("AOC-2024-10", () => {
@@ -54,6 +86,26 @@ describe("AOC-2024-10", () => {
   });
 
   it("part1-full returns trailhead score sum", () => {
+    expect(getTrailheadScoreSum(fullInput)).toEqual(468);
+  });
+
+  it("part2-test1 returns trailhead rating", () => {
+    expect(getTrailheadScoreSum(part2TestInput1)).toEqual(2);
+  });
+
+  it("part2-test2 returns trailhead rating", () => {
+    expect(getTrailheadScoreSum(part2TestInput2)).toEqual(4);
+  });
+
+  it("part2-test3 returns trailhead rating", () => {
+    expect(getTrailheadScoreSum(part2TestInput3)).toEqual(3);
+  });
+
+  it("part2-test4 returns trailhead rating", () => {
+    expect(getTrailheadScoreSum(part2TestInput4)).toEqual(36);
+  });
+
+  it("part2-full returns trailhead rating", () => {
     expect(getTrailheadScoreSum(fullInput)).toEqual(468);
   });
 });
