0a32ec66059537cb935fc88a6a2dfcc6ed47f6a7
ivan-burda <burda.ivan@protonmail.com>
2024-12-28 20:26:07 +0100

    2024-25-p1

--- 2024/25/2024-25.ts
+++ 2024/25/2024-25.ts
@@ -1,3 +1,53 @@
+type Key = string[][];
+type Lock = string[][];
+
+const isKeyMatchingLock = (lock: Lock, key: Key): boolean => {
+  let result = true;
+  for (let keyY = 0; keyY < key.length; keyY++) {
+    for (let keyX = 0; keyX < key[keyY].length; keyX++) {
+      if (key[keyY][keyX] === "#" && lock[keyY][keyX] === "#") {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
+
+const getKeysAndLocks = (
+  input: string,
+): Record<"keys" | "locks", Key[] | Lock[]> => {
+  return input.split(/\r?\n\r?\n/).reduce<{ locks: Lock[]; keys: Key[] }>(
+    (separatedParts, currentPart) => {
+      if (currentPart.startsWith("#####")) {
+        separatedParts.locks.push(
+          currentPart
+            .split(/\n/)
+            .map((line) => line.replaceAll(".", "0").split("")),
+        );
+      } else {
+        separatedParts.keys.push(
+          currentPart
+            .split(/\n/)
+            .map((line) => line.replaceAll(".", "0").split("")),
+        );
+      }
+      return separatedParts;
+    },
+    { keys: [], locks: [] },
+  );
+};
+
 export const getFittingLockKeyPairsCount = (input: string): number => {
-  return 0;
+  const keysAndLocks = getKeysAndLocks(input);
+
+  const compatibleKeyLockCombinations: string[] = [];
+  for (let k = 0; k < keysAndLocks.keys.length; k++) {
+    for (let l = 0; l < keysAndLocks.locks.length; l++) {
+      if (isKeyMatchingLock(keysAndLocks.locks[l], keysAndLocks.keys[k])) {
+        compatibleKeyLockCombinations.push(`K${k}-L${l}`);
+      }
+    }
+  }
+
+  return compatibleKeyLockCombinations.length;
 };
--- 2024/25/__tests__/2024-25.test.ts
+++ 2024/25/__tests__/2024-25.test.ts
@@ -40,13 +40,14 @@ const testInput = `#####
 #.#..
 #.#.#
 #####`;
+
 const fullInput = readFileSync("2024/25/2024-25.txt", "utf8");
 
 describe("AOC-2024-25", () => {
   it("part1-test returns fitting lock-key pairs count", () => {
     expect(getFittingLockKeyPairsCount(testInput)).toBe(3);
   });
-  xit("part1-full returns fitting lock-key pairs count", () => {
-    expect(getFittingLockKeyPairsCount(fullInput)).toBe(1);
+  it("part1-full returns fitting lock-key pairs count", () => {
+    expect(getFittingLockKeyPairsCount(fullInput)).toBe(3116);
   });
 });
